0eedf2b8656634bbf4fe381983e45bdc66f164b2
Fernando <fernando.perez.lara@outlook.com>
2020-06-02 22:04:27 +0200

    Program interface created

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Slide_Kinect {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
