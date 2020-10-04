cccb6554d42f9d8af9fc48a044a52b9903ce3818
Fernando Perez <fernando.perez.lara@outlook.com>
2020-10-04 16:44:40 +0200

    Improved code and commented in the camera reader class

--- Utilities/CameraReader.cs
+++ Utilities/CameraReader.cs
@@ -12,14 +12,17 @@ namespace Slide_Kinect {
         [DllImport("user32.dll")] // Library used to create a virtual keyboard
 
         public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo); // Virtual keyboard
-        public static bool changeSlide = false;
-        public static int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
-        public static int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
-        public static double minVirtualWidth = -0.3;
-        public static double maxVirtualWidth = 0.4;
-        public static double minVirtualHeight = -0.2;
-        public static double maxVirtualHeight = 0.4;
+        public static bool changeSlide = false; // Activated when slide is changing
 
+        // Virtual position properties
+        public struct virtualProperties {
+            public static double[] widthRange = new double[] { -0.3, 0.4 };
+            public static double[] heightRange = new double[] { -0.2, 0.4 };
+            public static int screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            public static int screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+        };
+
+        // Returns a color frame
         public static ImageSource kinectOutput(this ColorFrame frame) {
             int width = frame.FrameDescription.Width;
             int height = frame.FrameDescription.Height;
@@ -34,6 +37,7 @@ namespace Slide_Kinect {
             return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, data, width * PixelFormats.Bgr32.BitsPerPixel / 8);
         }
 
+        // Returns an infrared frame
         public static ImageSource kinectOutput(this InfraredFrame frame) {
             int width = frame.FrameDescription.Width;
             int height = frame.FrameDescription.Height;
@@ -54,6 +58,7 @@ namespace Slide_Kinect {
             return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, data, width * PixelFormats.Bgr32.BitsPerPixel / 8);
         }
 
+        // Returns a depth frame
         public static ImageSource kinectOutput(this DepthFrame frame) {
             int width = frame.FrameDescription.Width;
             int height = frame.FrameDescription.Height;
@@ -76,39 +81,47 @@ namespace Slide_Kinect {
             return BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, data, width * PixelFormats.Bgr32.BitsPerPixel / 8);
         }
 
+        // Gets the position of the skeleton
         public static void bodyPosition(this Canvas cnv_Video, BodyFrame frame, Label lbl_WorldXRight, Label lbl_WorldYRight, Label lbl_WorldZRight, Label lbl_WorldXLeft, Label lbl_WorldYLeft, Label lbl_WorldZLeft, Label lbl_RelativeXRight, Label lbl_RelativeYRight, CheckBox cbx_Skeleton, CheckBox cbx_NextSlide, CheckBox cbx_PreviousSlide, CheckBox cbx_CursorMode) {
-            IList<Body> bodies = new Body[frame.BodyFrameSource.BodyCount];
-            IReadOnlyDictionary<JointType, Joint> joints;
+            IList<Body> bodies = new Body[frame.BodyFrameSource.BodyCount]; // Create a body class
+            IReadOnlyDictionary<JointType, Joint> joints; // Create the joints
 
-            frame.GetAndRefreshBodyData(bodies);
+            frame.GetAndRefreshBodyData(bodies); // Get and refresh the skeleton
 
             foreach (Body body in bodies) {
                 if (body != null && body.IsTracked) {
                     joints = body.Joints;
-
+                    
+                    // Display world position on labels
                     lbl_WorldXRight.Content = (joints[JointType.HandTipRight].Position.X).ToString("0.00");
                     lbl_WorldYRight.Content = (joints[JointType.HandTipRight].Position.Y).ToString("0.00");
                     lbl_WorldZRight.Content = (joints[JointType.HandTipRight].Position.Z).ToString("0.00");
 
+                    // Display world position on labels
                     lbl_WorldXLeft.Content = (joints[JointType.HandTipLeft].Position.X).ToString("0.00");
                     lbl_WorldYLeft.Content = (joints[JointType.HandTipLeft].Position.Y).ToString("0.00");
                     lbl_WorldZLeft.Content = (joints[JointType.HandTipLeft].Position.Z).ToString("0.00");
 
-                    if ((joints[JointType.HandTipRight].Position.X >= minVirtualWidth) && (joints[JointType.HandTipRight].Position.X <= maxVirtualWidth)) {
-                        lbl_RelativeXRight.Content = (screenWidth * (-minVirtualWidth + joints[JointType.HandTipRight].Position.X) / (maxVirtualWidth - minVirtualWidth)).ToString("0");
+                    // Display relative position on labels
+                    if ((joints[JointType.HandTipRight].Position.X >= virtualProperties.widthRange[0]) && (joints[JointType.HandTipRight].Position.X <= virtualProperties.widthRange[1])) {
+                        lbl_RelativeXRight.Content = (virtualProperties.screenWidth * (-virtualProperties.widthRange[0] + joints[JointType.HandTipRight].Position.X) / (virtualProperties.widthRange[1] - virtualProperties.widthRange[0])).ToString("0");
                     }
 
-                    if ((joints[JointType.HandTipRight].Position.Y >= minVirtualHeight) && (joints[JointType.HandTipRight].Position.Y <= maxVirtualHeight)) {
-                        lbl_RelativeYRight.Content = (screenHeight * (-minVirtualHeight + joints[JointType.HandTipRight].Position.Y) / (maxVirtualHeight - minVirtualHeight)).ToString("0");
+                    // Display relative position on labels
+                    if ((joints[JointType.HandTipRight].Position.Y >= virtualProperties.heightRange[0]) && (joints[JointType.HandTipRight].Position.Y <= virtualProperties.heightRange[1])) {
+                        lbl_RelativeYRight.Content = (virtualProperties.screenHeight * (-virtualProperties.heightRange[0] + joints[JointType.HandTipRight].Position.Y) / (virtualProperties.heightRange[1] - virtualProperties.heightRange[0])).ToString("0");
                     }
 
+                    // Read hands movement
                     readHands(joints, cnv_Video, cbx_NextSlide, cbx_PreviousSlide, cbx_CursorMode);
 
+                    // Draw the skeleton on the display
                     if (cbx_Skeleton.IsChecked == true) {
                         foreach (Joint joint in body.Joints.Values) {
-                            cnv_Video.drawNode(joint);
+                            cnv_Video.drawNode(joint); // Draw nodes
                         }
 
+                        // Draw joints
                         cnv_Video.drawLine(body.Joints[JointType.Head], body.Joints[JointType.Neck]);
                         cnv_Video.drawLine(body.Joints[JointType.Neck], body.Joints[JointType.SpineShoulder]);
                         cnv_Video.drawLine(body.Joints[JointType.SpineShoulder], body.Joints[JointType.ShoulderLeft]);
@@ -138,40 +151,46 @@ namespace Slide_Kinect {
             }
         }
 
+        // Read hands movement
         public static void readHands(IReadOnlyDictionary<JointType, Joint> joints, Canvas cnv_Video, CheckBox cbx_NextSlide, CheckBox cbx_PreviousSlide, CheckBox cbx_CursorMode) {
-            CameraSpacePoint leftHand, leftElbow;
+            CameraSpacePoint leftHand, leftElbow, rightHand, rightElbow;
 
+            // Set joints position
             leftHand = joints[JointType.HandLeft].Position;
             leftElbow = joints[JointType.ElbowLeft].Position;
+            rightHand = joints[JointType.HandRight].Position;
+            rightElbow = joints[JointType.ElbowRight].Position;
 
+            // Check if activation mode is ready (secondary arm on 90 degrees)
             if ((Math.Abs(leftHand.X - leftElbow.X) <= 0.05) && (leftHand.Y > leftElbow.Y) && (Math.Abs(leftHand.Z - leftElbow.Z) <= 0.1)) {
-                shakeHand(joints[JointType.HandRight].Position, joints[JointType.ElbowRight].Position, cbx_NextSlide, cbx_PreviousSlide, cbx_CursorMode);
+                shakeHand(rightHand, rightElbow, cbx_NextSlide, cbx_PreviousSlide, cbx_CursorMode); // Detect the movement of the main hand
             }
         }
 
+        // Detect movement of the primary hand
         public static void shakeHand(CameraSpacePoint rightHand, CameraSpacePoint rightElbow, CheckBox cbx_NextSlide, CheckBox cbx_PreviousSlide, CheckBox cbx_CursorMode) {
-            if ((rightElbow.X - rightHand.X) >= 0.2) {
+            if ((rightElbow.X - rightHand.X) >= 0.2) { // Move the right hand to the left
                 if (changeSlide == false) {
                     if (cbx_NextSlide.IsChecked == true) {
-                        keybd_event((byte)0x27, 0, 0x0001 | 0, 0);
+                        keybd_event(0x27, 0, 0x0001 | 0, 0); // Right arrow key
                     }
                     changeSlide = true;
                 }
-            } else if ((rightElbow.X - rightHand.X) <= -0.2) {
+            } else if ((rightElbow.X - rightHand.X) <= -0.2) { // Move the right hand to the right
                 if (changeSlide == false) {
                     if (cbx_PreviousSlide.IsChecked == true) {
-                        keybd_event((byte)0x25, 0, 0x0001 | 0, 0);
+                        keybd_event(0x25, 0, 0x0001 | 0, 0); // Left arrow key
                     }
                     changeSlide = true;
                 }
-            } else if ((Math.Abs(rightElbow.Z - rightHand.Z) >= 0.3) && (Math.Abs(rightHand.Y - rightElbow.Y) <= 0.1) && (Math.Abs(rightHand.X - rightElbow.X) <= 0.1)) {
+            } else if ((Math.Abs(rightElbow.Z - rightHand.Z) >= 0.3) && (Math.Abs(rightHand.Y - rightElbow.Y) <= 0.1) && (Math.Abs(rightHand.X - rightElbow.X) <= 0.1)) { // Move hand forward
                 if (changeSlide == false) {
                     if (cbx_CursorMode.IsChecked == true) {
                         // Cursor mode activated
                     }
                     changeSlide = true;
                 }
-            } else {
+            } else { // It is at rest
                 if (changeSlide == true) {
                     changeSlide = false;
                 }
